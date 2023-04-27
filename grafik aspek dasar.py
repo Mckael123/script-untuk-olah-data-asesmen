d3441fb86a87d8b5182014881ca277e944159810
McKael-forti <rafiddharma95@gmail.com>
2023-04-27 08:42:29 +0700

    new update

--- grafik aspek dasar.py
+++ grafik aspek dasar.py
@@ -2,11 +2,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # import plotly.express as px
 # contoh data
-data= pd.read_excel("untuk laporan.xlsx",sheet_name='LAPORAN')
+data= pd.read_excel("untuk laporan.xlsx",sheet_name='tes')
 
 # Data
-categories = ['Pengelolaan Stres', 'Adaptasi Sosial', 'Manajemen Waktu', 'Motivasi Berprestasi']
+categories = ['Motivasi Berprestasi',' Motivasi Berprestasi','Manajemen Waktu',' Manajemen Waktu','Adaptasi Sosial',' Adaptasi Sosial','Pengelolaan Stres',' Pengelolaan Stres']
 values = []
+#colors=['#128FC8','#D31929']
+#colors=['#FEBE03','#AB274F'] #semi fix
+colors=['#75A9CC','#FCDDBC']
 for index, row in data.iterrows():
     # Create horizontal bar chart
     nilai_Motivasi=row['Motivasi Berprestasi']
@@ -15,10 +18,10 @@ for index, row in data.iterrows():
     nilai_stress=row['Pengelolaan Stress']
 
     nama_siswa=row['Nama']
-    values = [nilai_Motivasi,nilai_waktu,nilai_sosial,nilai_stress]
+    values = [nilai_Motivasi,3,nilai_waktu,2.5,nilai_sosial,2.5,nilai_stress,3]
     #print(values)
-    plt.figure(figsize=(12,4.5))
-    plt.barh(categories, values,color='#128FC8',align='center')
+    plt.figure(figsize=(12.5,4.5))
+    plt.barh(categories, values,color=colors,align='center')
     
     nama_siswa=row['Nama']
 
@@ -28,6 +31,6 @@ for index, row in data.iterrows():
    #plt.ylabel('Category')
 
     # Show plot
-    plt.savefig("D:\projek aac/tes maba\input\grafik dasar/GD-test_maba-"+nama_siswa+".png",transparent=True)
-    #plt.show()
+    plt.savefig("D:\projek aac/tes maba\input\GD-test_maba-"+nama_siswa+".png",transparent=True)
+    plt.show()
     plt.close()
