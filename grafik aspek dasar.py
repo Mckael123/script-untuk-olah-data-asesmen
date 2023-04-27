52e05e70186434e1be4a1f7504e19bd5f6e36773
McKael-forti <rafiddharma95@gmail.com>
2023-04-27 11:39:37 +0700

    update

--- grafik aspek dasar.py
+++ grafik aspek dasar.py
@@ -2,11 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # import plotly.express as px
 # contoh data
-<<<<<<< HEAD
-data= pd.read_excel("untuk laporan.xlsx",sheet_name='tes')
-=======
+
 data= pd.read_excel("recap_name.xlsx",sheet_name='Nama_Sheet')#yang diubah yang recap_name dan Nama_sheeet sesuai file masing- masing
->>>>>>> f0d0868ece7a3a1a27cc6fc3449c03e227f1d301
+
 
 # Data
 categories = ['Motivasi Berprestasi',' Motivasi Berprestasi','Manajemen Waktu',' Manajemen Waktu','Adaptasi Sosial',' Adaptasi Sosial','Pengelolaan Stres',' Pengelolaan Stres']
@@ -35,11 +33,11 @@ for index, row in data.iterrows():
    #plt.ylabel('Category')
 
     # Show plot
-<<<<<<< HEAD
+
     plt.savefig("D:\projek aac/tes maba\input\GD-test_maba-"+nama_siswa+".png",transparent=True)
     plt.show()
-=======
+
     plt.savefig("D:\direktori-saving-file/firstName_file-"+nama_siswa+".png",transparent=True)#yang dirubah "lokasi direktori dan nama file
     #plt.show()
->>>>>>> f0d0868ece7a3a1a27cc6fc3449c03e227f1d301
+
     plt.close()
