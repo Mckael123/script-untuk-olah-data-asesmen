678be13ebe8cf73d26274dea332b45b8286e8ce0
McKael-forti <76393985+Mckael123@users.noreply.github.com>
2024-05-09 21:09:46 +0700

    Update grafikk gaya belajar.py

--- grafikk gaya belajar.py
+++ grafikk gaya belajar.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-# import plotly.express as px
-# contoh data
+
+# import data
 data= pd.read_excel("Contoh_merger_rekap.xlsx",sheet_name='Nama_Sheet')#dapat diubah nama file excell dan nama sheet pada file masing-masing
 
-# Data
+# initate variable word attribute Data
 categories = ['Visual', 'Auditory', 'Linguistik', 'Physical','Logika','Sosial','Intrapersonal']
 values = []
+#loop make graph for every row in dataset
 for index, row in data.iterrows():
-    # Create horizontal bar chart
+   
     nilai_Visual=row['VISUAL']
     nilai_Auditory=row['AUDITORY']
     nilai_Linguistik=row['LINGUISTIK']
@@ -18,7 +19,8 @@ for index, row in data.iterrows():
     nilai_Intrapersonal=row['INTRAPERSONAL']
     nama_siswa=row['Nama']
     values = [nilai_Visual,nilai_Auditory,nilai_Linguistik,nilai_Physical,nilai_Logika,nilai_Social,nilai_Intrapersonal]
-    #print(values)
+
+     # Create horizontal bar chart
     plt.figure(figsize=(8.5,4.5))
     plt.barh(categories, values,color='#F1A700',align='center')
     for i, v in enumerate(values):
@@ -26,12 +28,10 @@ for index, row in data.iterrows():
         plt.annotate(' ('+str(round(v/sum(values)*100, 1))+'%)', xy=(v-2, i), va='center', ha='left', fontsize=10,color='black', fontweight='bold')
     nama_siswa=row['Nama']
 
-    # Add title and labels
-    # plt.title('Horizontal Bar Chart')
-    #plt.xlabel('score')
-   #plt.ylabel('Category')
 
-    # Show plot
+
+    # Save plot
     plt.savefig("D:\lokasi-file-direktori/FirstName_file-"+nama_siswa+".png",transparent=True)#dapat diganti lokasi direkotri penyimpanan file dan nama depan file
-    #plt.show()
+    #show plot
+    plt.show()
     plt.close()
